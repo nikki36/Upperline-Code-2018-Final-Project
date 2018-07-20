ef4fe2f43084a4574c58ecd3294b5d75a0c2e174
Samuel Anderson <samuela820@gmail.com>
2018-07-20 18:16:52 +0000

    changed

---
+++ game.js
@@ -0,0 +1,23 @@
+// const player = document.querySelector('#player');
+
+// player.addEventListener('collide', e => {
+//     if(lowerPlane.id == e.detail.body.el.id) {
+//         console.log("Collided into the box!");
+//     }  
+    
+//     console.log("Collider (below):");
+//     console.log(e.detail.target.el);
+//     console.log("Collider ID: " + e.detail.target.el.id);
+    
+//     console.log("Collided with (below):");
+//     console.log(e.detail.body.el);
+    
+//     e.detail.target.el;  // Original entity, the collider
+//     e.detail.body.el;    // Entity that was collided with
+//     e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
+//     e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+// }); 
+
+//  if collision with lowest plane, respawn to highest plane and fall on repeat!!!
+
+alert("Cross the Bridges and Pass the Blue Columns. If you can!");
